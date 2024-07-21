16cf2c58aa9ae1db07d4138e1fafb8632795adbe
Finasic <ffinasic@gmail.com>
2024-07-21 12:03:24 -0700

    update2

--- metal-app/ContentView.swift
+++ metal-app/ContentView.swift
@@ -7,18 +7,24 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MeetingView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    MeetingView()
 }
