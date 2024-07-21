f8cddfe5a7d55fe166508ac74b618bf4d162ba67
Finasic <ffinasic@gmail.com>
2024-07-21 11:59:21 -0700

    Initial Commit

---
+++ metal-app/metal_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  metal_appApp.swift
+//  metal-app
+//
+//  Created by Eric Qin on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct metal_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
